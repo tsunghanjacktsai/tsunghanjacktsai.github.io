93e98ef5e43c4cdf9c2bbc19eacf9b4e6d461565
jack870131 <tsaitsunghan@apache.org>
2022-01-23 14:27:11 +0800

    Site updated: 2022-01-23 14:27:10

--- js/tw_cn.js
+++ js/tw_cn.js
@@ -102,3 +102,103 @@ function translateInitilization() {
 		}
 	}
 }
+
+// var defaultEncoding = 2; // 网站默认语言，1: 繁體中文, 2: 简体中文
+// var translateDelay = 0; //延迟时间,若不在前, 要设定延迟翻译时间, 如100表示100ms,默认为0
+// var cookieDomain = "https://jack870131.github.io/"; //更改为你的博客网址
+// var translateButtonId = "translateLink"; //默认互换id
+// var currentEncoding = defaultEncoding;
+// var targetEncodingCookie = "targetEncoding" + cookieDomain.replace(/\./g, "");
+// var targetEncoding = (getCookie(targetEncodingCookie) == null ? defaultEncoding: getCookie(targetEncodingCookie));
+// var translateButtonObject;
+
+// function translateText(txt) {
+// 	if (txt == "" || txt == null) return "";
+// 	if (currentEncoding == 1 && targetEncoding == 2) return Simplized(txt);
+// 	else if (currentEncoding == 2 && targetEncoding == 1) return Traditionalized(txt);
+// 	else return txt
+// }
+// function translateBody(fobj) {
+// 	if (typeof(fobj) == "object") var objs = fobj.childNodes;
+// 	else var objs = document.body.childNodes;
+// 	for (var i = 0; i < objs.length; i++) {
+// 		var obj = objs.item(i);
+// 		if ("||BR|HR|TEXTAREA|".indexOf("|" + obj.tagName + "|") > 0 || obj == translateButtonObject) continue;
+// 		if (obj.title != "" && obj.title != null) obj.title = translateText(obj.title);
+// 		if (obj.alt != "" && obj.alt != null) obj.alt = translateText(obj.alt);
+// 		if (obj.tagName == "INPUT" && obj.value != "" && obj.type != "text" && obj.type != "hidden") obj.value = translateText(obj.value);
+// 		if (obj.nodeType == 3) obj.data = translateText(obj.data);
+// 		else translateBody(obj)
+// 	}
+// }
+// function translatePage() {
+// 	if (targetEncoding == 1) {
+// 		currentEncoding = 1;
+// 		targetEncoding = 2;
+// 		// translateButtonObject.innerHTML = msgToTraditionalChinese;
+// 		setCookie(targetEncodingCookie, targetEncoding, 7);
+// 		translateBody()
+// 	} else if (targetEncoding == 2) {
+// 		currentEncoding = 2;
+// 		targetEncoding = 1;
+// 		// translateButtonObject.innerHTML = msgToSimplifiedChinese;
+// 		setCookie(targetEncodingCookie, targetEncoding, 7);
+// 		translateBody()
+// 	}
+// }
+// function Traditionalized(cc) {
+// 	var str = '';
+// 	const OpenCC = require('opencc');
+// 	const converter = new OpenCC('s2twp.json');
+// 	converter.convertPromise(cc).then(converted => {
+// 	  // console.log(converted);  // 漢字
+// 	  str += converted;
+// 	});
+// 	return str
+// }
+// function Simplized(cc) {
+// 	var str = '';
+// 	const OpenCC = require('opencc');
+// 	const converter = new OpenCC('tw2sp.json');
+// 	converter.convertPromise(cc).then(converted => {
+// 	  // console.log(converted);  // 漢字
+// 	  str += converted;
+// 	});
+// 	return str
+// }
+// function setCookie(name, value, days) {
+// 	if (days) {
+// 		var date = new Date();
+// 		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+// 		var expires = "; expires=" + date.toGMTString()
+// 	} else var expires = "";
+// 	document.cookie = name + "=" + value + expires + "; path=/"
+// }
+// function getCookie(name) {
+// 	var nameEQ = name + "=";
+// 	var ca = document.cookie.split(';');
+// 	for (var i = 0; i < ca.length; i++) {
+// 		var c = ca[i];
+// 		while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+// 		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length)
+// 	}
+// 	return null
+// }
+// function translateInitilization() {
+// 	translateButtonObject = document.getElementById(translateButtonId);
+// 	if (translateButtonObject) {
+// 		with(translateButtonObject) {
+// 			if (typeof(document.all) != "object") {
+// 				href = "javascript:translatePage();"
+// 			} else {
+// 				href = "#";
+// 				onclick = new Function("translatePage(); return false;")
+// 			}
+// 		}
+// 		if (currentEncoding != targetEncoding) {
+// 			setTimeout("translateBody()", translateDelay);
+// 			// if (targetEncoding == 1) translateButtonObject.innerHTML = msgToSimplifiedChinese;
+// 			// else translateButtonObject.innerHTML = msgToTraditionalChinese
+// 		}
+// 	}
+// }
